0de992f31c9d5fba66f98a28b5b38f430fddbc65
Oscar Hugo Cardenas Lopez <ohcl87@hotmail.com>
2018-09-20 16:03:08 -0500

    limpiando script

--- install.sh
+++ install.sh
@@ -1,31 +1,21 @@
 #!bin/bash
 
 rm -rf ~/.vimrc ~/.vimrc.plugins ./tmp
-
 Directory=./tmp
 CurrentDirectory=$(pwd)
 mkdir $Directory
 mkdir $Directory/swap
 mkdir $Directory/backup
 
-sudo apt-add-repository ppa:brightbox/ruby-ng
-sudo add-apt-repository ppa:jonathonf/vim
-sudo apt-get update
-sudo apt-get install curl ruby2.3 ruby2.3-dev build-essential python-dev python3-dev libssl-dev vim-nox -y
 npm install -g jshint
-
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
 cp .vimrc ~/
 cp .vimrc.plugins ~/
-
-#plugins
 vim +PlugUpdate +qall
 
 cd ./plugins/command-t/ruby/command-t/ext/command-t
 ruby extconf.rb && make
-
 cd $CurrentDirectory
 
 vim -u NONE -c "helptags ./plugins/vim-fugitive/doc" -c q
